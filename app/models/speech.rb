ea2f3a55de858a22c8cef1daab5679807f2a7eb8
Taylor Mravic <taylor.mravic@gmail.com>
2022-03-07 12:38:27 +0900

    total seconds method on speech model

--- app/models/speech.rb
+++ app/models/speech.rb
@@ -3,10 +3,16 @@ class Speech < ApplicationRecord
   belongs_to :training
   has_one_attached :audio, dependent: :destroy
 
+  PRICE_PER_SECOND = 5
+
   def seconds
     return 0 unless length
 
     (length.fdiv(1000)).round(0)
+  end
+
+  def total_price
+    PRICE_PER_SECOND * seconds
   end
 
   # callback
