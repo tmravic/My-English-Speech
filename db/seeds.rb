6f4f0299660afae9240e3ec8a17a636a21b20295
andrei mori <andrei.code.nz@gmail.com>
2022-03-08 16:15:36 +0900

    Scooter

--- db/seeds.rb
+++ db/seeds.rb
@@ -153,30 +153,66 @@ u.photo.attach(io: file, filename: 'avatar.jpeg', content_type: 'image/jpeg')
 u.save!
 puts "user created"
 
+users = User.all.to_a
+
 t = Training.create!( user_id: u.id )
 t.save!
 puts "Training created"
 
-s = Speech.new(transcript: ' Hello everyone' , notes: ' This is my speech ', user_id: u.id, training_id: t.id )
+s1 = Speech.new(transcript: ' I am thrilled to welcome the Class of 2022 to our Graduation Party. I can scarcely believe we are graduates – it really is a time for a huge celebration. I love the party venue already, it is outstanding!
+  Before we launch the event let us take a moment to thank the party organising committee, the team who set up the party and all those who are helping tonight. This party has been many months in the planning phase and many weeks in preparation. The team also raised a lot of money to help towards costs. We all owe you a lot.
+  Now, one way we can show our appreciation is to have the time of our lives!
+  There is great music, dancing, entertainers, food, drinks, snack kiosks, lots of things to see and do all round the venue and of course there is our Class to be with – what more can you need.
+  I have to tell you for safety the exits are signposted in case of emergency, if you need help contact one of the helpers right away and also for comfort the rest rooms are also signposted. Be sure to check out where everything is.
+  Congratulations to all of us on our graduation. Tonight is our moment to celebrate wildly and to enjoy the company of our Class.
+  I hope we have the night of our lives!' , notes: ' This is my graduation speech, any feedback appreciated. ', user_id: users.first.id, training_id: t.id )
 file = URI.open('https://res.cloudinary.com/dpyl9bage/video/upload/v1646705662/development/ox3ps4nokhjokwezpdj5pklq72ii.webm')
-s.audio.attach(io: file, filename: 'speech_audio.webm', content_type: 'video/webm')
-s.save!
-puts "Speech created"
+s1.audio.attach(io: file, filename: 'speech_audio.webm', content_type: 'video/webm')
+s1.save!
+puts "Speech created #{s1}"
 
-# https://res.cloudinary.com/dpyl9bage/video/upload/v1646705662/development/ox3ps4nokhjokwezpdj5pklq72ii.wav
+users.shift
 
-# https://res.cloudinary.com/dpyl9bage/video/upload/v1646705662/development/ox3ps4nokhjokwezpdj5pklq72ii.mp3
+s2 = Speech.new(transcript: 'Welcome to the Graduation Party of a lifetime for the graduating Class of 2022.
+  We have a party lined up that you will be talking to your grandchildren about.
+  There is music from the best band in the Kyoto Prefecture.
+  There is great food, drinks, snacks, plus donut and bagel kiosks. We have wondering entertainers all around the party. Plus games booths where you can win prizes. We have dance prizes and lots of spot prizes too.
+  Just get right into the party and see where it takes you. I am sure you will have the time of your lives.
+  Congratulations Class of 2022.' , notes: ' Urgent! If possible I would like it done by tomorrow. ', user_id: users.first.id, training_id: t.id )
+file = URI.open('https://res.cloudinary.com/dpyl9bage/video/upload/v1646705662/development/ox3ps4nokhjokwezpdj5pklq72ii.webm')
+s2.audio.attach(io: file, filename: 'speech_audio.webm', content_type: 'video/webm')
+s2.save!
+puts "Speech created #{s2}"
 
-# CREATING FAKE SPEECHES
+users.shift
 
-# user.each do |speeches|
-#     s = Speech.new(
-#       title: ,
-#       length: (1..10).sample,
-#       transcript:
-#       notes:
-#       user: User.offset(rand(User.count)).first
-#     )
+s3 = Speech.new(transcript: 'It is possible to imagine that human nature, the human intellect, emotions and feelings are completely independent of our technologies; that we are essentially ahistorical beings with one constant human nature that has remained the same throughout history or even pre-history? Sometimes evolutionary psychologists—those who belief human nature was fixed on the Pleistocene Savannah—talk this way. I think this is demonstrably wrong.
+  Consider ancient technologies, for example: cooking. When ancient hunter-gatherers discovered that firing their meat would not only make it tastier but could make their food easier to digest, it had a number of knock-on effects. It made parts of an animal carcass that were previously inedible edible, and also made it possible to preserve food longer. But this was not only a culinary but a cognitive success.Why? Because the amount of time our ancestors had to spend finding, hunting, and butchering their food, and thus maintain life, could be reduced making time available to invest in other activities such as thinking. Thanks to cooking, new time became available to prehistoric humans to plan, to consider, and invent other even more liberating technologies—or even to fritter away drawing bison and mammoth on the walls of caves.
+  ' , notes: ' Hi, I would like some help with intonation mainly.  ', user_id: users.first.id, training_id: t.id )
+file = URI.open('https://res.cloudinary.com/dpyl9bage/video/upload/v1646705662/development/ox3ps4nokhjokwezpdj5pklq72ii.webm')
+s3.audio.attach(io: file, filename: 'speech_audio.webm', content_type: 'video/webm')
+s3.save!
+puts "Speech created #{s3}"
 
-#     s.save!
+# s = Speech.new(transcript: 'Hello everyone.
+#   My name is Jim Smith, and I'm a systems analyst.
+#   I've been with the company for 5 years now. I started off as a technician and over the years progressed to my current role.
+#   When I'm not working, I like to spend time with my son. We do a lot of fishing and camping together. My wife isn't a big fan, but she tags along with us most of the time.
+#   It's good to be here and to meet you all in person. We all exchange emails and phone calls, but it's hard to really know someone until you've met face to face. I'm looking forward to talking about our upcoming project with all of you'
+#   , notes: ' This is a short self introduction that I   ', user_id: u.id, training_id: t.id )
+# file = URI.open('https://res.cloudinary.com/dpyl9bage/video/upload/v1646705662/development/ox3ps4nokhjokwezpdj5pklq72ii.webm')
+# s.audio.attach(io: file, filename: 'speech_audio.webm', content_type: 'video/webm')
+# s.save!
+# puts "Speech created"
+
+
+
+
+
+# def random_user
+#   User.offset(rand(User.count)).first
+# end
+
+# def random_speech
+#   Pet.offset(rand(Speech.count)).first
 # end
