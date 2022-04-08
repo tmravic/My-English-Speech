9ea21d4d0bc2943e81ee658de25f44bafd6a618b
Taylor Mravic <taylor.mravic@gmail.com>
2022-04-08 12:26:15 +0900

    ja.yml added

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module MyEnglishSpeech
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :en
+    config.i18n.fallbacks = true
   end
 end
